03069eb2cdf190ed13b4e9bf46277270c018f564
RyanClinton777 <58789023+RyanClinton777@users.noreply.github.com>
2020-12-15 22:19:08 +0000

    Update country functionallity, installed body-parser

--- MySQLDAO.js
+++ MySQLDAO.js
@@ -1,6 +1,6 @@
 //This file is a DAO (Data access object) - We use it as an abstract interface for our DB
 //Imports
-const { promiseImpl } = require("ejs"); //Promise implimentation from ejs (?)
+const { promiseImpl } = require("ejs"); //Promise implementation - used to return promises
 var mysql = require("promise-mysql"); //A wrapper for the MySQLJS driver that allows us to use promises.
 
 //https://www.npmjs.com/package/promise-mysql
@@ -26,15 +26,22 @@ mysql.createPool({
         console.log(error);
     });
 
-var getCountries = function () {
+//return country with given id, or all if id is undefined.
+var getCountries = function (id) {
+
+    var i = 1;
+    var i = 1;
     //return a promise for this query so it can run asynchronously
     return new Promise((resolve, reject) => {
+        //if no id, return all, else get that country of that id. Used for list page.
+        myQuery = (id == undefined ? "select * from country" : "select * from country where co_code = ?;");
+
         //We use ? for value placeholders to prevent sql injection attacks.
         //The driver knows to scrub the values and put them into the ?s in order
         //putting these in an object is a just a neater equivilent of passing the two strings as seperate args 
         var queryObj = {
-            sql: "select * from country;",
-            values: ""
+            sql: myQuery,
+            values: [id]
         }
 
         pool.query(queryObj)
@@ -49,5 +56,26 @@ var getCountries = function () {
     });
 }
 
+//update country with given id, details
+var updateCountry = function (id, name, details) {
+    //return promise
+    return new promiseImpl((resolve, reject) => {
+        //compose query, using params 
+        var queryObj = {
+            sql: "UPDATE country SET co_name = ?, co_details = ? WHERE co_code = ?",
+            values: [name, details, id]
+        }
+
+        //Attempt to perform query
+        pool.query(queryObj)
+            .then((result) => {
+                resolve(result);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+}
+
 //Export functions
-module.exports = { getCountries };
+module.exports = { getCountries, updateCountry };
--- index.js
+++ index.js
@@ -2,11 +2,17 @@
 var express = require("express"); //back-end web app framework
 var ejs = require("ejs"); //viewing/template engine, used to send views;
 var mySQLDAO = require("./MySQLDAO.js"); //Import our MYSQL DAO
+//body parser middle-ware, to parse and get the data from the body of a request.
+var bodyParser = require("body-parser");
 
 var app = express(); //handle used to access express methods
 
 //Set view engine
 app.set("view engine", "ejs");
+
+//set bodyParser
+//https://www.npmjs.com/package/body-parser#bodyparserurlencodedoptions
+app.use(bodyParser.urlencoded({ extended: false }))
 
 //listen on port 3007
 app.listen(3007, () => {
@@ -23,11 +29,49 @@ app.get("/ListCountries", (req, res) => {
     //getStudents returns a promise, so we need to handle that here
     mySQLDAO.getCountries(req.params.id)
         .then((result) => {
+            //console.log(result);
             //Render our view, passing in the results into an array in that file.
             //We don't need to specify .ejs, it knows.
             res.render("showCountries", { countries: result });
         })
         .catch((error) => {
+            res.send(error);
+        });
+});
+
+//edit country form; takes co_code in as parameter in the url to make sure the country still exists, and to display the current values.
+app.get("/edit/:id", (req, res) => {
+    //pass in country code from URL ()
+    mySQLDAO.getCountries(req.params.id)
+        //if promise is resolved it sends back result
+        .then((result) => {
+            //Empty result sets won't be rejected/return error, so we check here.
+            if (result.length > 0) {
+                //render editCountry view, create a variable called country with the results of the query, which will be the data for the country of given id.
+                //result is an array, even though there is necassarily only one result
+                res.render("editCountry", { country: result[0] });
+            }
+            else {
+                res.send("<h3>No such student with ID " + req.params.id + "</h3>");
+            }
+        })
+        //ditto but rejection, and error
+        .catch((error) => {
+            res.send(error);
+        });
+});
+``
+//update country record. Takes data from the body of the request by using body-parser
+app.post("/updateCountry", (req, res) => {
+    console.log("UPDATE: " + req.body.code + ", " + req.body.name + ", " + req.body.details);
+
+    //Update, pass in new details
+    mySQLDAO.updateCountry(req.body.code, req.body.name, req.body.details)
+        .then((result => {
+            //redirect back to list page
+            res.redirect("/ListCountries");
+        }))
+        .catch( (error) => {
             res.send(error);
         });
 });
